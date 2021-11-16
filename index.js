6270bc593810ce88f797c93e77d2c574905ec63e
Cecep Muhammad Ridwan <88409866+ibnahmad@users.noreply.github.com>
2021-11-16 19:07:33 +0700

    delete API folder

--- index.js
+++ index.js
@@ -49,37 +49,6 @@ app.get('/register', ( req, res)=>{
 
 
 
-var artist = members.artist
-console.log(artist[1].name)
-
-
-app.get('/api/artist', (req, res)=>{
-  const artist = members.artist
-    let data = " "
-    for(let i = 0; i< artist.length; i++){
-
-        data += `<h4>${artist[i].name}</h4>`
-    }
-        res.send(data)    
-})
-
-
-app.get('/home/justin/album', (req, res)=>{
-    const offMyFace = members.offMyFace[0].tracks
-    let arr = ""
-    for(let i = 0; i < offMyFace.length; i++){
-        arr += `<h4>${offMyFace[i]}<h4/>`
-    }
-    res.send(arr);
-})
-
-
-app.get('/api/artist/album?', (req, res)=>{
-    
-    res.json(artist);
-})
-
-
 
 app.listen(PORT, ()=>{
     console.log(`running in port http://localhost:${PORT}/`);
